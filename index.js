9618d7a1428f87b5223790a91568d1949484138b
Shane Jampole <shane.jampole.98@gmail.com>
2023-03-07 23:19:19 -0500

    committing loop labwork

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const name = []
+function writeCards(name, message){
+    const array = []
+    //const nameLength = name.length
+    for(let i = 0; i <= name.length -1; i++){
+        array.push(`Thank you, ${name[i]}, for the wonderful ${message} gift!`)
+    }
+    return array
+}
+function countDown(n){
+    let count = n
+    while(count >= 0){
+        console.log(count)
+        count--
+    } 
+}
